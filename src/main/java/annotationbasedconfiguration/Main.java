aa7e30314609d213f5ae801521bd1ba9cccef224
tamler98 <120762094+tamler98@users.noreply.github.com>
2022-12-28 00:25:50 +0700

    Update Main.java

--- src/main/java/annotationbasedconfiguration/Main.java
+++ src/main/java/annotationbasedconfiguration/Main.java
@@ -5,8 +5,6 @@ import pojo.AccountService;
 
 public class Main {
     public static void main(String[] args) {
-//        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("xmlbasedconfiguration/bean.xml");
-//        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("annotationbasedconfiguration/beans.xml");
         AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
 
